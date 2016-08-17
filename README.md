1778e17a4abb7e70f938786f8d07bb5f889913e6
Risto Saarelma <risto.saarelma@iki.fi>
2016-08-17 06:05:06 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Hardware scancode conversion tables
+
+Will convert keyboard layout independent hardware keyboard scancodes on
+Windows, Linux or OS X platforms into standard values.
---
+++ src/lib.rs
@@ -0,0 +1,168 @@
+/// Default values for hardware keyboard keys.
+///
+/// After USB HID Usage Tables document at http://www.usb.org/developers/hidpage/Hut1_12v2.pdf
+#[derive(Copy, Clone, Eq, PartialEq, Debug)]
+pub enum Scancode {
+    A = 4,
+    B,
+    C,
+    D,
+    E,
+    F,
+    G,
+    H,
+    I,
+    J,
+    K,
+    L,
+    N,
+    M,
+    O,
+    P,
+    Q,
+    R,
+    S,
+    T,
+    U,
+    V,
+    W,
+    X,
+    Y,
+    Z,
+    Num1 = 30,
+    Num2,
+    Num3,
+    Num4,
+    Num5,
+    Num6,
+    Num7,
+    Num8,
+    Num9,
+    Num0,
+    Enter = 40,
+    Escape,
+    Backspace,
+    Tab,
+    Space,
+    Minus,
+    Equals,
+    LeftBracket,
+    RightBracket,
+    Backslash,
+    NonUsHash = 50,
+    Semicolon,
+    Apostrophe,
+    Grave,
+    Comma,
+    Period,
+    Slash,
+    CapsLock,
+    F1 = 58,
+    F2,
+    F3,
+    F4,
+    F5,
+    F6,
+    F7,
+    F8,
+    F9,
+    F10,
+    F11,
+    F12,
+    PrintScreen = 70,
+    ScrollLock,
+    Pause,
+    Insert,
+    Home,
+    PageUp,
+    Delete,
+    End,
+    PageDown,
+    Right,
+    Left,
+    Down,
+    Up,
+    NumLock = 83,
+    PadDivide,
+    PadMultiply,
+    PadMinus,
+    PadPlus,
+    PadEnter,
+    Pad1 = 89,
+    Pad2,
+    Pad3,
+    Pad4,
+    Pad5,
+    Pad6,
+    Pad7,
+    Pad8,
+    Pad9,
+    Pad0,
+    PadDecimal,
+    NonUsBackslash = 100,
+    Application,
+    Power,
+    PadEquals,
+    F13,
+    F14,
+    F15,
+    F16,
+    F17,
+    F18,
+    F19,
+    F20,
+    F21,
+    F22,
+    F23,
+    F24,
+    Execute = 116,
+    Help,
+    Menu,
+    Select,
+    Stop,
+    Again,
+    Undo,
+    Cut,
+    Copy,
+    Paste,
+    Find,
+    Mute,
+    VolumeUp,
+    VolumeDown,
+    SysReq = 154,
+    LeftControl = 224,
+    LeftShift,
+    LeftAlt,
+    LeftGui,
+    RightControl,
+    RightShift,
+    RightAlt,
+    RightGui,
+}
+
+#[cfg(target_os = "macos")]
+mod scancode_macos;
+#[cfg(target_os = "linux")]
+mod scancode_linux;
+#[cfg(target_os = "windows")]
+mod scancode_windows;
+
+mod scancode {
+    #[cfg(target_os = "macos")]
+    pub use scancode_macos::MAP;
+    #[cfg(target_os = "linux")]
+    pub use scancode_linux::MAP;
+    #[cfg(target_os = "windows")]
+    pub use scancode_windows::MAP;
+}
+
+impl Scancode {
+    /// Try to convert a hardware scancode from the current platform to a Scancode enum value.
+    pub fn new(hardware_scancode: u8) -> Option<Scancode> {
+        if (hardware_scancode as usize) < scancode::MAP.len() {
+            scancode::MAP[hardware_scancode as usize]
+        } else {
+            None
+        }
+    }
+}
